9bf51ee8eab0a9881d8c7725047694a055f4e607
Rhyan Lugo Crespo <rhyanlugocrespo@gmail.com>
2023-08-29 00:04:18 -0400

    Implemented all parts of the profile card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,64 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="gnome.jpg" alt="gnome child" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Rhyan Lugo Crespo</h1>
+      <p>
+        Computer Science student at the University of Florida with a planned
+        graduation of Fall 2023. When not coding or working on school, I like to
+        play video games, play ttrpgs, read, and cook.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="C++" color="gray" />
+      <Skill skill="C#" color="green" />
+      <Skill skill="Java" color="red" />
+      <Skill skill="HTML + CSS" color="purple" />
+      <Skill skill="Javascript/Typescript" color="orange" />
+      <Skill skill="React" color="blue" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+    </div>
+  );
+}
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
